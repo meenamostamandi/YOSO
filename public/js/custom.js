b16c2490b84faf7b2d15e5af5fcc93e81e8b1bef
Albert Ke <albertke1991@gmail.com>
2014-02-12 15:59:33 -0800

    Added tab changing functionality

--- public/js/custom.js
+++ public/js/custom.js
@@ -8,10 +8,19 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+var currentTab = "lists";
+
 function initializePage() {
 	console.log("Javascript connected!");
 	$(".list-all .list-obj").click(displayList);
 	$(".left-nav").click(displayAll);
+
+	$(".todo").click(toggleCheck);
+	$(".checked").click(toggleCheck);
+
+	$(".tab").hide();
+	$("#lists-tab").show();
+	$(".tab-icon").click(changeTab);
 }
 
 function displayList(e) {
@@ -26,4 +35,32 @@ function displayAll(e) {
 	e.preventDefault();
 	$(".list-area").hide();
 	$(".list-all").show();
+}
+
+function toggleCheck(e) {
+	if ($(this).is(".todo")) {
+		$(this).removeClass("todo").addClass("checked");
+		$(this).find(".glyphicon-unchecked").removeClass("glyphicon-unchecked").addClass("glyphicon-ok");
+	}
+	else {
+		$(this).removeClass("checked").addClass("todo");
+		$(this).find(".glyphicon-ok").removeClass("glyphicon-ok").addClass("glyphicon-unchecked");
+	}
+}
+
+function changeTab(e) {
+	var nextTab = $(this).attr("id");
+	nextTab = nextTab.substring(0, nextTab.length - 5);
+	console.log(currentTab + ", " + nextTab);
+	if (nextTab != currentTab) {
+		console.log("changing tabs");
+
+		$("#" + currentTab + "-icon").removeClass("selected");
+		$("#" + nextTab + "-icon").addClass("selected");
+
+		$("#" + currentTab + "-tab").hide();
+		$("#" + nextTab + "-tab").show();
+
+		currentTab = nextTab;
+	}
 }
